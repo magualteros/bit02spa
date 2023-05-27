06ea22c4f71418ef59c27bc0b91cb5e542f8849e
magp96 <marlon.Agualteros@gmail.com>
2023-05-26 20:05:10 -0500

    Primer publicacón

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import {Nav} from './assets/components/Nav'
+
+function App() {
+
+  return (
+    <>
+      <Nav/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Nav.jsx
@@ -0,0 +1,14 @@
+export const Nav = () => {
+    
+    return (
+        <>
+        <nav>
+            <ul>
+                <li>Inicio</li>
+                <li>Contacto</li>
+                <li>Registro</li>
+            </ul>
+        </nav>
+    </>
+    );
+}
